b43bce76b88995c7a0ac74e906cb9dc2deea8f16
Pedro Quinonez <pedroquinonez0@gmail.com>
2014-12-04 16:32:47 -0700

    Added restart/continue buttons

--- level1.lua
+++ level1.lua
@@ -7,6 +7,7 @@
 local composer = require( "composer" )
 local scene = composer.newScene()
 local ragdogLib = require ("ragdogLib")
+local widget = require( "widget" )
 local physics = require( "physics" )
 physics.start( )
 physics.setGravity( 0, 0 )
@@ -28,6 +29,12 @@ local current = 0
 local highText
 local currentScore
 local currentText
+local leaderboards
+local play
+local restart
+local continue
+local gameOver
+local rectangleBg
 local circle
 local triangleOne
 local triangleTwo
@@ -36,24 +43,54 @@ local triangleFour
 local color = 1 --Used to keep track of the triangle color
 local rando = math.random(1,4) --Random number used to color ball
 local ballMove = false
+local multiplier = 1
+local multiplierText
 
 --FUNCTIONS
 function moveBall(event)
 
 	--if ballMove == true then
 	if circle.y > _H/2-42 then
-		circle.y = math.random(-100, 0)
+		circle.y = math.random(-200, 0)
+		circle:setLinearVelocity( 0, 0 )
 		if circle.number == color then
-			current = current + 1
+			circle:applyLinearImpulse( 0, math.random(15,30), circle.x, circle.y )
+			current = current + 1 * multiplier
 			currentText.text = current
+		else
+			transition.to(highScore, {time = 400, x=_W/2-(_W/4), y= 50})
+			transition.to(highText, {time = 400, x=_W/2-(_W/4), y= 95})
+			transition.to(currentScore, {time = 400, x=_W/2+(_W/4), y= 50})
+			transition.to(currentText, {time = 400, x=_W/2+(_W/4), y= 95})
+			--transition.to(rectangleBg, {time = 400, x=_W/2})
+			transition.to(rectangleBg, {time = 200, alpha = 0.8})
+			--rectangleBg.alpha = .8
+			transition.to(continue, {time = 400, x=_W/2})
+			transition.to(leaderboards, {time = 400, x=_W/2})
+			transition.to(restart, {time = 400, x=_W/2})
 		end
-		function updateScore( event )
+		function updateBall( event )
 			rando = math.random(1,4)
 			circle.number = rando
 			circle:setFillColor( ragdogLib.convertHexToRGB(_G.ballPalette[rando]) )
 		end
-		timer.performWithDelay( 100, updateScore ,1 )
+		timer.performWithDelay( 100, updateBall ,1 )
 	end
+end
+
+function continuePress ( event )
+	--stuff
+	return true
+end
+
+function leaderboardsPress ( event )
+	--stuff
+	return true
+end
+
+function restartPress( event )
+	
+	return true
 end
 
 function scene:create( event )
@@ -71,22 +108,72 @@ function scene:create( event )
 
 	highScore = display.newText("H I G H  S C O R E", 0, 0, "Bebas Neue", 20)
 	highScore.x = _W/2-(_W/4)
-	highScore.y = 50
-	highScore.alpha = 0
+	highScore.y = -120
+	highScore:setFillColor(65/255, 65/255, 65/255)
+	--highScore.alpha = 0
 
-	highText = display.newText( high, 0, 0, "04B_19", 50)
+	highText = display.newText( high, 0, 0, "04B_19", 70)
 	highText.x = highScore.x
-	highText.y = highScore.y+30
-	highText.alpha=0
+	highText.y = highScore.y+45
+	--highText.alpha=0
 
 	currentScore = display.newText("S C O R E", 0, 0, "Bebas Neue", 20)
 	currentScore.x = _W/2+(_W/4)
-	currentScore.y = 50
-	currentScore.alpha = 0
+	currentScore.y = -120
+	currentScore:setFillColor(65/255, 65/255, 65/255)
+	--currentScore.alpha = 0
 
 	currentText = display.newText( current, 0, 0, "04B_19", 70)
 	currentText.x = _W/2
 	currentText.y = 100
+
+	continue = widget.newButton{
+		label="C O N T I N U E ?",
+		font="Bebas Neue",
+		labelColor = { default={255}, over={128} },
+		fontSize=38,
+		--defaultFile="button.png",
+		--overFile="button-over.png",
+		width=154, 
+		height=40,
+		onRelease = continuePress	-- event listener function
+	}	
+	continue.x = -120
+	continue.y = _H/2-40
+
+	leaderboards = widget.newButton{
+		label="G A M E  C E N T E R",
+		font="Bebas Neue",
+		labelColor = { default={255}, over={128} },
+		fontSize=38,
+		--defaultFile="button.png",
+		--overFile="button-over.png",
+		width=154, 
+		height=40,
+		onRelease = leaderboardsPress	-- event listener function
+	}	
+	leaderboards.x = -120
+	leaderboards.y = _H/2
+
+	restart = widget.newButton{
+		label="R E S T A R T",
+		font="Bebas Neue",
+		labelColor = { default={255}, over={128} },
+		fontSize=38,
+		--defaultFile="button.png",
+		--overFile="button-over.png",
+		width=154, 
+		height=40,
+		onRelease = restartPress	-- event listener function
+	}	
+	restart.x = -120
+	restart.y = _H/2+40
+
+	rectangleBg = display.newRect( 0, 0, _W, _H )
+	rectangleBg.x = _W/2
+	rectangleBg.y = _H/2
+	rectangleBg:setFillColor( ragdogLib.convertHexToRGB("#000000") )
+	rectangleBg.alpha = 0
 	
 	triangleOne = display.newImageRect(  "images/triangle.png", 130, 65 )
 	triangleOne.x = _W/2
@@ -111,24 +198,28 @@ function scene:create( event )
 	triangleFour.y = triangleThree.y-42
 	triangleFour:setFillColor( ragdogLib.convertHexToRGB(_G.ballPalette[4]) )
 
-	circle = display.newCircle( 0, 0, 10 )
+	circle = display.newImageRect(  "images/circle.png", 20, 20 )
 	circle.x = _W/2
 	circle.y = 50
 	circle:setFillColor( ragdogLib.convertHexToRGB(_G.ballPalette[rando]) )
-	physics.addBody( circle, {radius=10,density=1, friction=1, bounce=1, isSensor = true } )
+	physics.addBody( circle, {radius=10,density=10, friction=1, bounce=1, isSensor = true } )
 	circle.number = rando
 
 	-- all display objects must be inserted into group
 	sceneGroup:insert( background )
-	sceneGroup:insert( highScore )
-	sceneGroup:insert( highText )
-	sceneGroup:insert( currentScore )
-	sceneGroup:insert( currentText )
 	sceneGroup:insert( circle )
 	sceneGroup:insert( triangleOne )
 	sceneGroup:insert( triangleTwo )
 	sceneGroup:insert( triangleThree )
 	sceneGroup:insert( triangleFour )
+	sceneGroup:insert( rectangleBg )
+	sceneGroup:insert( highScore )
+	sceneGroup:insert( highText )
+	sceneGroup:insert( currentScore )
+	sceneGroup:insert( currentText )
+	sceneGroup:insert( continue )
+	sceneGroup:insert( leaderboards )
+	sceneGroup:insert( restart )
 end
 
 
@@ -170,7 +261,7 @@ function scene:show( event )
 		-- INSERT code here to make the scene come alive
 		-- e.g. start timers, begin animation, play audio, etc.
 		background:addEventListener("touch", flipSquare)
-		circle:applyLinearImpulse( 0, math.random(.5,2.0), circle.x, circle.y )
+		circle:applyLinearImpulse( 0, math.random(5,20), circle.x, circle.y )
 		Runtime:addEventListener("enterFrame", moveBall)
 	end
 end
@@ -193,8 +284,8 @@ end
 
 function scene:destroy( event )
 
-	-- Called prior to the removal of scene's "view" (sceneGroup)
-	-- 
+	-- 	-- Called prior to the removal of scene's "view" (sceneGroup)
+
 	-- INSERT code here to cleanup the scene
 	-- e.g. remove display objects, remove touch listeners, save state, etc.
 	local sceneGroup = self.view
